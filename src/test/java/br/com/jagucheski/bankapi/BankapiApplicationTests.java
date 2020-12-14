b27f35dae6f3f4912690200d9c17cdf280e467f7
jagucheski <maj.ski@hotmail.com>
2020-12-14 12:20:06 -0300

    Initial Commit

---
+++ src/test/java/br/com/jagucheski/bankapi/BankapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jagucheski.bankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
